cc75a81768c3cdcbd47d1ee3c5fbc7cbd6f21a1d
haritssm <38557477+haritssm@users.noreply.github.com>
2019-04-11 17:26:33 +0700

    Add files via upload

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -4,52 +4,63 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
-#include<sys/wait.h>
 
-pthread_t tid[3]; //inisialisasi array untuk menampung thread dalam kasus ini ada 2 thread
+pthread_t thread;
+pthread_t tid[50];
 
-int length=5; //inisialisasi jumlah untuk looping
+int array[50];
 
 //sort
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
- 
-void sort(int arr[], int n) 
+void* sort(void* n) //sortnya pakai bubble sort
 {
-   int i, j; 
-   for (i = 0; i < n-1; i++){    
-       for (j = 0; j < n-i-1; j++){
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+	int z = *(int *)n; //karena n tipenya void diubah menjadi integer
+	int i, j; 
+	for (i = 0; i < z-1; i++)
+	{
+       for (j = 0; j < z-i-1; j++)
+	   {
+           if (array[j] > array[j+1])
+           {
+           		int swap = array[j];
+           		array[j]=array[j+1];
+           		array[j+1]=swap;
+		   }
        }
    }
 }
 
 //factorial
-unsigned int* factorial(unsigned int n) 
+void* factorial(void* n) 
 {
-    if (n == 0) 
-    return 1; 
-    return n * factorial(n - 1);
+	int a = *(int *)n;
+    int num=a;
+        unsigned long long sum=1;
+        while(a>0)
+		{
+            sum=sum*a;
+            a--;
+        }
+        printf("%d! = %llu\n", num, sum);
 }
 
 int main(int argc, char* argv[])
 {
-	int array[50];
-    for(i=1; i<argc; i++){
-        array[i]= atoi(argv[i]);
+	int i;
+    for(i=1; i<argc; i++)
+	{
+        array[i-1] = atoi(argv[i]); //masukin nilai argv ke dalam array
+        							//pakai atoi karena argv tipenya char, diubah menjadi integer
     }
     
-    sort(array, argc);
-    //while(i--){
-    //    printf("%d", array[i]);
-    //    }
-
-    for(i=1; i<argc; i++){
-        printf("%ud", factorial(array[i]))
+    int batas = argc-1;
+    pthread_create (&thread, NULL, &sort, &batas);
+    pthread_join(thread, NULL); //buat nunggu nge-sort dulu, baru di faktorial
+    /*int y;
+    for(y=0; y<batas; y++) printf("%d ", array[y]);
+    printf("\n");*/
+    for(i=0; i<argc-1; i++)
+	{
+    	pthread_create(&(tid[i]), NULL, &factorial, &array[i]);
+    	pthread_join(tid[i], NULL); //buat nunggu faktorialnya selesai satu-satu
     }
 }
